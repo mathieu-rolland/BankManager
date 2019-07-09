734283d2d501ee22a429acde9ff9d2df6c081ffb
Mathieu Rolland <mathieu.rolland2705@gmail.com>
2019-07-09 21:02:14 +0200

    Ajout des dates lors de la création manuelle d'une opération.

--- bank-manager-gui/src/components/Operation/OperationCreateComponent.vue
+++ bank-manager-gui/src/components/Operation/OperationCreateComponent.vue
@@ -5,21 +5,31 @@
         <b-button v-b-modal="'collapse-create-operation'" variant="primary">Nouvelle opération</b-button>
 
         <b-modal id="collapse-create-operation" class="mt-2" ref="modal" @ok="createOperation" title="Créer une opération">
+            
             <form method="post" id="category-create-form">
+            
                 <label for="label">Libellé</label>
                 <b-form-input id="label" v-model="label" type="text" name="label" placeholder="Entrer le libellé" />
+            
                 <label for="amount">Montant</label>
                 <b-form-input id="amount" v-model="amount" type="number" name="amount" placeholder="Entrer le montant" />
+                    
+                <label for="way">Date</label>
+                <datepicker :value="operationDate" :input-class="'datepicker-input'"></datepicker>
+
                 <label for="way">Sens</label>
                 <b-form-select name="way" v-model="selected_way">
                     <option value="DEBIT">Débit</option>
                     <option value="CREDIT">Crédit</option>
                 </b-form-select>
+
                 <label for="category">Catégorie</label>
                 <b-form-select name="category" v-model="selected_category">
                     <option v-for="category in categories" v-bind:value="category.id">{{category.name}}</option>
                 </b-form-select>
+
             </form>
+            
         </b-modal>
     </div>
 
@@ -28,10 +38,16 @@
 <script>
 
 import axios from 'axios';
+import Datepicker from 'vuejs-datepicker';
+import moment from 'moment';
 
 export default {
 
     name: 'CreateOperationComponent',
+
+    components:{
+        Datepicker
+    },
 
     props: {
         categories: Array,
@@ -43,11 +59,14 @@ export default {
             label: "",
             amount: 0,
             selected_way: "",
-            selected_category: ""
+            selected_category: "",
+            operationDate: new Date(),
        } 
     },
 
     methods: {
+
+
         createOperation: function(){
             var self = this;
             var selectedCategoryObject = undefined;
@@ -63,7 +82,8 @@ export default {
                     label: this.label,
                     amount: this.amount,
                     category: selectedCategoryObject,
-                    operationWay: this.selected_way
+                    operationWay: this.selected_way,
+                    date: moment(this.operationDate).set({hour:0,minute:0,second:0,millisecond:0}).format()
                 })
                 .then( this.callback )
                 .catch(function (error) {
@@ -75,3 +95,22 @@ export default {
 
 }
 </script>
+
+<style>
+
+    .datepicker-input{
+        display: block;
+        width: 100%;
+        height: calc(1.5em + .75rem + 2px);
+        padding: .375rem .75rem;
+        font-size: 1rem;
+        font-weight: 400;
+        line-height: 1.5;
+        color: #495057;
+        background-color: #fff;
+        background-clip: padding-box;
+        border: 1px solid #ced4da;
+        border-radius: .25rem;
+        transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+    }
+</style>
--- src/main/java/com/perso/bank/services/OperationService.java
+++ src/main/java/com/perso/bank/services/OperationService.java
@@ -2,15 +2,13 @@ package com.perso.bank.services;
 
 import java.io.File;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
-
-import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.perso.bank.config.LocalDateTimeConfig;
 import com.perso.bank.domain.dto.OperationDTO;
 import com.perso.bank.domain.dto.convert.OperationConverter;
 import com.perso.bank.repository.OperationRepository;
@@ -35,17 +33,12 @@ public class OperationService {
 	@Autowired
 	private AutoAffectService autoAffectService;
 	
-	private DateTimeFormatter dateTimeFormatter;
-	
-	@PostConstruct
-	public void setup() {
-//		dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-uuuu'THH:mm:ss");
-		dateTimeFormatter = DateTimeFormatter.ISO_ZONED_DATE_TIME;
-	}
+	@Autowired
+	private LocalDateTimeConfig localDateTimeConfig;
 	
 	public List<OperationDTO> getWithDate(String date){
 		
-		LocalDateTime startDate = LocalDateTime.parse(date , dateTimeFormatter).withDayOfMonth(1);
+		LocalDateTime startDate = LocalDateTime.parse(date , localDateTimeConfig.getDateTimeFormatter() ).withDayOfMonth(1);
 		LocalDateTime endDate = startDate.plusMonths(1).minusDays(1);
 		
 		log.debug("Recherche des operations entre {} et {}." , startDate , endDate);
